96a9fb80d3a4f9be39bb5c4f929f517e31c85d06
MikeDacanay <mikedacanay6@gmail.com>
2024-06-10 16:09:24 +0800

    Initial Commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,210 @@
+<template>
+  <q-layout view="lhh Lpr lff">
+    <q-header elevated class="headerClass q-pl-lg q-pr-xl">
+      <q-toolbar style="max-width: 95%">
+        <q-btn flat round class="q-ma-lg">
+          <q-avatar size="55px">
+            <img src="~assets/starbucks.png">
+          </q-avatar>
+        </q-btn>
+
+        <div class="gt-sm">
+          <q-btn flat color="dark" label="MENU" class="custom-hover text-weight-bold text-7" />
+          <q-btn flat color="dark" label="REWARDS" class="custom-hover text-weight-bold text-h7" />
+          <q-btn flat color="dark" label="GIFT CARDS" class="custom-hover text-weight-bold text-h7" />
+        </div>
+
+        <q-space />
+
+        <div class="q-mr-lg q-gutter-md gt-sm">
+          <q-btn flat color="dark" label="Find a store" class="text-weight-bold q-mr-xl" icon="location_on" no-caps />
+          <q-btn rounded outline color="dark" label="Sign In" class="text-weight-bold" no-caps />
+          <q-btn rounded color="dark" label="Join Now" class="text-weight-bold" no-caps />
+        </div>
+
+        <div class="lt-md">
+          <q-btn flat dense round icon="menu" aria-label="Menu" @click="drawerOpen = !drawerOpen" color="primary" />
+        </div>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="drawerOpen" :width="250" side="right">
+      <q-list>
+        <q-item clickable exact v-ripple to="/" active-class="q-item-active">
+          <q-item-section avatar>
+            <q-icon name="list" />
+          </q-item-section>
+          <q-item-section>
+            Todo
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable exact v-ripple to="/help" active-class="q-item-active">
+          <q-item-section avatar>
+            <q-icon name="help" />
+          </q-item-section>
+          <q-item-section>
+            Help
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-footer elevated class="q-pt-lg bg-white">
+      <div class="row flex justify-start q-px-lg">
+        <div class="column inline q-ml-lg" style="max-width: 190px; width: 190px">
+          <div class="text-dark q-mb-md" style="font-size: 19px">
+            About Us
+          </div>
+          <a class="q-py-sm q-mb-sm" href=" #">Our Company</a>
+          <a class="q-py-sm q-mb-sm" href="#">Our Coffee</a>
+          <a class="q-py-sm q-mb-sm" href="#">Stories and News</a>
+          <a class="q-py-sm q-mb-sm" href="#">Starbucks Archive</a>
+          <a class="q-py-sm q-mb-sm" href="#">Investor Relations</a>
+          <a class="q-py-sm q-mb-sm" href="#">Customer Service</a>
+          <a class="q-py-sm q-mb-sm" href="#">Contact Us</a>
+        </div>
+        <div class="column inline q-ml-lg" style="max-width: 190px;width: 190px">
+          <div class="text-dark q-mb-md" style="font-size: 19px">
+            Careers
+          </div>
+          <a class="q-py-sm q-mb-sm" href=" #">Culture and Values</a>
+          <a class="q-py-sm q-mb-sm" href="#">Inclusion, Diversity, and Equity</a>
+          <a class="q-py-sm q-mb-sm" href="#">College Achievement Plan</a>
+          <a class="q-py-sm q-mb-sm" href="#">Alumni Community</a>
+          <a class="q-py-sm q-mb-sm" href="#">U.S. Careers</a>
+          <a class="q-py-sm q-mb-sm" href="#">Customer Service</a>
+          <a class="q-py-sm q-mb-sm" href="#">International Careers</a>
+        </div>
+        <div class="column inline q-ml-lg" style="max-width: 190px;width: 190px">
+          <div class="text-dark q-mb-md" style="font-size: 19px">
+            Social Impact
+          </div>
+          <a class="q-py-sm q-mb-sm" href=" #">People</a>
+          <a class="q-py-sm q-mb-sm" href="#">Planet</a>
+          <a class="q-py-sm q-mb-sm" href="#">Environmental and Social Impact Reporting</a>
+        </div>
+        <div class="column inline q-ml-lg" style="max-width: 190px;width: 190px">
+          <div class="text-dark q-mb-md" style="font-size: 19px">
+            For Business Partners
+          </div>
+          <a class="q-py-sm q-mb-sm" href=" #">Landlord Support Center</a>
+          <a class="q-py-sm q-mb-sm" href="#">Suppliers</a>
+          <a class="q-py-sm q-mb-sm" href="#">Corporate Gift Card Sales</a>
+          <a class="q-py-sm q-mb-sm" href="#">Office and Foodservice Coffee</a>
+        </div>
+        <div class="column inline q-ml-lg" style="max-width: 190px;width: 190px">
+          <div class="text-dark q-mb-md" style="font-size: 19px">
+            Order and Pick Up
+          </div>
+          <a class=" q-py-sm q-mb-sm" href=" #">Order on the App</a>
+          <a class="q-py-sm q-mb-sm" href="#">Order on the Web</a>
+          <a class="q-py-sm q-mb-sm" href="#">Delivery</a>
+          <a class="q-py-sm q-mb-sm" href="#">Order and Pick Up Options</a>
+          <a class="q-py-sm q-mb-sm" href="#">Explore and Find Coffee for Home</a>
+        </div>
+      </div>
+
+      <q-separator class="q-my-lg" />
+
+      <ul class="flex row ">
+        <li>
+          <q-btn unelevated color="dark" text-color="white" round size="12px">
+            <q-icon name="fa-brands fa-spotify" />
+          </q-btn>
+        </li>
+        <li>
+          <q-btn unelevated color="dark" text-color="white" round size="12px">
+            <q-icon name="fa-brands fa-facebook" />
+          </q-btn>
+        </li>
+        <li>
+          <q-btn unelevated color="dark" text-color="white" round size="12px">
+            <q-icon name="fa-brands fa-pinterest" />
+          </q-btn>
+        </li>
+        <li>
+          <q-btn unelevated color="dark" text-color="white" round size="12px">
+            <q-icon name="fa-brands fa-instagram" />
+          </q-btn>
+        </li>
+        <li>
+          <q-btn unelevated color="dark" text-color="white" round size="12px">
+            <q-icon name="fa-brands fa-youtube" />
+          </q-btn>
+        </li>
+        <li>
+          <q-btn unelevated color="dark" text-color="white" round size="12px">
+            <q-icon name="fa-brands fa-twitter" />
+          </q-btn>
+        </li>
+      </ul>
+
+      <ul class="column inline q-mt-md">
+        <a class="footerText q-py-md q-mb-xs" href="">Privacy Notice</a>
+        <a class="footerText q-py-md q-mb-xs" href="">Consumer Health Privacy Notice</a>
+        <a class="footerText q-py-md q-mb-xs" href="">Terms of Use</a>
+        <a class="footerText q-py-md q-mb-xs" href="">Do Not Share My Personal Information</a>
+        <a class="footerText q-py-md q-mb-xs" href="">CA Supply Chain Act</a>
+        <a class="footerText q-py-md q-mb-xs" href="">Accessibility</a>
+        <a class="footerText q-py-md q-mb-xs" href="">Cookie Preferences</a>
+      </ul>
+
+      <p class="text-dark q-ml-lg" style="opacity: 0.6">© 2024 Starbucks Coffee Company. All rights reserved.</p>
+
+    </q-footer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const drawerOpen = ref(false)
+
+defineOptions({
+  name: 'MainLayout'
+})
+
+</script>
+
+<style lang="scss">
+.header-image {
+  height: 200%;
+  z-index: -1;
+}
+
+li {
+  display: inline-block;
+  position: relative;
+  margin-right: 9px;
+}
+
+.footerText {
+  opacity: 1 !important;
+}
+
+a {
+  text-decoration: none;
+  font-size: 16px;
+  opacity: 0.6;
+  color: #000;
+}
+
+.headerClass {
+  background: white;
+}
+
+.custom-hover {
+  transition: color 0.5s;
+  font-size: large;
+}
+
+.custom-hover:hover {
+  color: #00704A !important;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,220 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="container1 flex items-center q-my-lg">
+      <div class="product">
+        <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-92442.jpg" alt="">
+      </div>
+      <div class="text-container">
+        <span class="text-h5 text-weight-bold text-primary q-pa-md">Enjoy half off your first
+          drink</span>
+        <div class="text-center text-primary text-h6 q-py-lg">
+          Download the app for half off a handcrafted drink during your first week.* Plus, tap into mobile ordering,
+          exclusive offers and so much more.
+        </div>
+        <q-btn rounded outline label="Sign up here" no-caps color="primary" class="signUp" />
+      </div>
+    </div>
+
+    <div class="container2 flex items-center q-my-lg">
+      <div class="text-container">
+        <span class="text-h3 text-weight-bold text-white text-center">Starbucks® Summer
+          Game</span>
+        <div class="text-center text-white text-h4 q-py-lg">
+          Play for a chance to win a trip for two to Costa Rica and millions of other prizes!**
+        </div>
+        <q-btn rounded outline label="Sign up here" no-caps color="white" class="signUp" />
+      </div>
+      <div class="product">
+        <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-92093.png" alt="">
+      </div>
+    </div>
+
+    <div class="container3 flex items-center q-my-lg">
+      <div class="product">
+        <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-92096.jpg" alt="">
+      </div>
+      <div class="text-container">
+        <span class="text-h3 text-weight-bold text-white text-center">Summertime crew</span>
+        <div class="text-center text-white text-h4 q-py-lg">
+          Super chill flavors, all lined up and ready for fun.
+        </div>
+        <q-btn rounded outline label="Order now" no-caps color="white" class="signUp" />
+      </div>
+    </div>
+
+    <div class="container4 flex items-center q-my-lg">
+      <div class="text-container">
+        <span class="text-h4 text-weight-bold text-dark text-center">The best offers are in the
+          app</span>
+        <div class="text-center text-dark text-h5 q-py-lg">
+          Get the app for refreshing offers this summer. Grab a friend, because we're treating you to special deals on
+          your go-to drinks and seasonal favorites.
+        </div>
+        <q-btn rounded outline label="Get the app" no-caps color="dark" class="signUp" />
+      </div>
+      <div class="product">
+        <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-92345.jpg" alt="">
+      </div>
+    </div>
+
+    <div class="container5 flex items-center q-my-lg">
+      <div class="product">
+        <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-92264.jpg" alt="">
+      </div>
+      <div class="text-container">
+        <span class="text-h5 text-weight-bold text-primary text-center">Summertime, when the
+          sippin’ (and saving) is easy</span>
+        <div class="text-center text-primary text-h6 q-py-lg">
+          Now through 6/14, you can get 20% off your first Starbucks order with DoorDash (up to $5 off).***
+        </div>
+        <q-btn rounded outline label="Order now" no-caps color="primary" class="signUp" />
+      </div>
+    </div>
+
+    <div class="container6 flex items-center q-my-lg">
+      <div class="text-container">
+        <span class="text-h4 text-weight-bold text-primary text-center">Personal cups for good</span>
+        <div class="text-center text-primary text-h5 q-py-lg">
+          Your choice is a positive and responsible one—because bringing your clean reusable cup helps our planet.
+        </div>
+        <q-btn rounded outline label="Learn more" no-caps color="primary" class="signUp" />
+      </div>
+      <div class="product">
+        <img src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-92108.jpg" alt="">
+      </div>
+    </div>
+
+    <div class="column q-pb-xl">
+      <div class="textBottom flex flex-center">
+        <div class="text-body2 text-weight-medium text-center q-pa-xl text-bottom1" style="max-width: 65%">
+          *50% off handcrafted drink valid for 7 days from sign up, at participating stores. Must sign up via promo link
+          by June 30, 2024 to be eligible. Limit one per member. Excludes delivery orders, alcohol, ready to drink and
+          bottled beverages.
+        </div>
+      </div>
+
+      <div class="textBottom flex flex-center">
+        <div class="text-body2 text-weight-medium text-center q-pa-xl text-bottom1" style="max-width: 65%">
+          **NO PURCHASE NECESSARY. Participating stores only. Starbucks Partners (employees) are not eligible to win
+          Sweepstakes prizes. Ends 6/17/24. To play and for <a href="#">Official Rules,</a> visit
+          <a href="">starbuckssummergame.com.</a>
+        </div>
+      </div>
+
+      <div class="textBottom flex flex-center">
+        <div class="text-body2 text-weight-medium text-center q-pa-xl text-bottom1" style="max-width: 65%">
+          ***20% off your first Starbucks order on DoorDash on 6/1/24 - 6/14/24 only. Max discount of $5. Fees and taxes
+          still apply. Limit one per customer. Menu limited. See the DoorDash app for availability & restrictions.
+        </div>
+      </div>
+    </div>
+
+
+
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
+
+<style scoped>
+.container1 {
+  align-items: center;
+  background-color: #c0e8a6;
+  max-width: 80%;
+}
+
+.container2 {
+  display: flex;
+  align-items: center;
+  background-color: #c300bd;
+  max-width: 80%;
+}
+
+.container3 {
+  display: flex;
+  align-items: center;
+  background-color: #0021db;
+  max-width: 80%;
+}
+
+.container4 {
+  display: flex;
+  align-items: center;
+  background-color: #bbf2d1;
+  max-width: 80%;
+}
+
+.container5 {
+  display: flex;
+  align-items: center;
+  background-color: #98cee3;
+  max-width: 80%;
+}
+
+.container6 {
+  display: flex;
+  align-items: center;
+  background-color: #97d8b1;
+  max-width: 80%;
+}
+
+.product {
+  flex: 1;
+}
+
+.product img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+
+.text-container {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.text-container .text-center {
+  max-width: 58%;
+  margin: 0 auto;
+}
+
+.signUp {
+  font-size: 16px;
+  margin-bottom: 20px;
+}
+
+@media (max-width: 768px) {
+
+  .container1,
+  .container3,
+  .container5 {
+    flex-direction: column;
+    max-width: 100%;
+  }
+
+  .product {
+    margin-bottom: 20px;
+  }
+
+  .text-container .text-center {
+    max-width: 100%;
+  }
+
+  .text-bottom1 {
+    max-width: 100% !important;
+  }
+
+  .container2,
+  .container4,
+  .container6 {
+    flex-direction: column-reverse;
+    max-width: 100%;
+  }
+}
+</style>
